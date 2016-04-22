1be7e6504bef632bc2567c5238c8c543761f8e80
Ji-Yuhang <yuhang.silence@gmail.com>
2016-04-23 03:19:11 +0800

    add boot-strap style

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,11 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!
   def home
   end
-
+  def about
+  end
   def help
   end
-
-  def about
+  def contact
   end
 end
