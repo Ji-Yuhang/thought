c7c1c488d17e1aaa6c4f5582aaa32e3c149eeec8
Ji-Yuhang <yuhang.silence@gmail.com>
2016-06-09 13:42:59 +0800

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ set :rbenv_ruby, '2.3.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
-
+set :unicorn_pid, "/var/www/thought/current/shared/pids/unicorn.pid"
 namespace :deploy do
 
   after :restart, :clear_cache do
