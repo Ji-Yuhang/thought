87bbb13b8ede45757a552b93dd03f5891c412863
Ji-Yuhang <yuhang.silence@gmail.com>
2016-10-26 18:45:20 +0800

    add thesaurus.dump

--- config/initializers/dict.rb
+++ config/initializers/dict.rb
@@ -1,5 +1,6 @@
 #$thesaurus = Marshal.load File.read "/Users/jiyuhang/test/ruby/nce/marshal_dump_verb.dump"
-$thesaurus = Marshal.load File.read "/Users/jyd/JianGuoYun/nce/marshal_dump_verb.dump"
+thesaurus_path = Rails.root.join('config', 'initializers' ,'marshal_dump_verb.dump')
+$thesaurus = Marshal.load File.read thesaurus_path
 $frequency = Hash.new
 $shanbay = Hash.new
 Word.each do |w|
